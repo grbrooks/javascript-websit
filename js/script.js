988af7b812409e41108876413bbc6e44948af0c5
Graham Brooks <grahambrooks@hotmail.co.uk>
2021-07-14 19:21:11 +0100

    --- Auto-Git Commit ---

---
+++ js/script.js
@@ -0,0 +1,9 @@
+function revealMessage() {
+    document.getElementById("hiddenMessage").style.display = "block";
+}
+
+function countDown() {
+    let currentVal = document.getElementById("countDownButton").innerHTML;
+    let newVal = currentVal - 1;
+    document.getElementById("")
+}
